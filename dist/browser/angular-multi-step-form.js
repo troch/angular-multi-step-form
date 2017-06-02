214e1b70922e36a529160e2153bcb36a8edb15cd
Julien Derenty <julien@boxmyjob.com>
2017-06-02 12:32:52 +0200

    dist

--- dist/browser/angular-multi-step-form.js
+++ dist/browser/angular-multi-step-form.js
@@ -135,16 +135,16 @@ var angularMultiStepForm = (function (angular$1) {
                       if (currentEnterAnimation) {
                           $animate.cancel(currentEnterAnimation);
                       }
-                      // Destroy current scope
-                      if (currentStepScope) {
-                          currentStepScope.$destroy();
-                      }
-                      // Leave current step if any
-                      if (currentStepElement) {
-                          currentLeaveAnimation = $animate.leave(currentStepElement);
-                      }
                       // Enter new step when new step element is ready
                       newStepElement.then(function (step) {
+                          // Destroy current scope
+                          if (currentStepScope) {
+                              currentStepScope.$destroy();
+                          }
+                          // Leave current step if any
+                          if (currentStepElement) {
+                              currentLeaveAnimation = $animate.leave(currentStepElement);
+                          }
                           onStepChange();
                           currentStepScope = step.scope;
                           currentStepElement = step.element;
